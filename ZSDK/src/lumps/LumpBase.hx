2a3b4d4680fdca933bebd907f6140df9dcab4de8
Kaelan <kbeevans@gmail.com>
2018-07-31 17:12:58 -0600

    LumpBase update

--- ZSDK/src/lumps/LumpBase.hx
+++ ZSDK/src/lumps/LumpBase.hx
@@ -8,7 +8,8 @@ import haxe.CallStack;
  */
 class LumpBase 
 {
-
+	public var id:Int; //If a lump allows for multiple definitions, IE custer 1, cluster 2, cluster 3, this var is for tracking that value
+	public var name:String; //If a lump allows for multiple definitions, IE actor_a, actor_b, actor_c, this var is for tracking that value
 	public function new() 
 	{
 		

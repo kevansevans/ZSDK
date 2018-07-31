22c3c55a7197ae42fc57d9273613a00c3977a85a
Kaelan <kbeevans@gmail.com>
2018-07-31 17:04:31 -0600

    Gameinfo base outline

---
+++ ZSDK/src/lumps/game/Gameinfo.hx
@@ -0,0 +1,94 @@
+package lumps.game;
+import flash.Vector;
+
+/**
+ * ...
+ * @author kevansevans
+ */
+enum abstract Order(Int) from Int {
+	
+}
+class Gameinfo extends LumpBase
+{
+	var definitions:Vector<Null<DefinitionBase>>;
+	public function new() 
+	{
+		super();
+	}
+}
+class Cluster extends DefinitionBase {
+	public function new() {
+		super();
+	}
+	
+}
+class Episode extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class Map extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class Skill extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class GameInfoDef extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class Intermission extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class Automap extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class EditorNumber extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class SpawnNumber extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class ConversationNumber extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}
+class DamageType extends DefinitionBase {
+	
+	public function new() {
+		super();
+	}
+	
+}

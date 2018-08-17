5ac6f6767a0f8a3e5092a556ccf5287d54753da5
Kaelan <kbeevans@gmail.com>
2018-08-17 13:33:12 -0600

    No UI yet but getting close

---
+++ ZSDK/src/lumps/UIBase.hx
@@ -0,0 +1,19 @@
+package lumps;
+
+import haxe.ui.containers.Box;
+
+/**
+ * ...
+ * @author kevansevans
+ */
+class UIBase extends Box
+{
+	public var lump:Lump;
+	public function new()
+	{
+		super();
+	}
+	public function process() {
+		
+	}
+}
---
+++ ZSDK/src/lumps/ui/r/Readme.hx
@@ -0,0 +1,19 @@
+package lumps.ui.r;
+
+import lumps.UIBase;
+
+/**
+ * ...
+ * @author kevansevans
+ */
+class Readme extends UIBase
+{
+
+	public function new() 
+	{
+		super();
+		
+		//this.lump = new Readme();
+	}
+	
+}

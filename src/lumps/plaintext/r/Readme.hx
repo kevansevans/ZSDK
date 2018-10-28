0a82381795c27cd81aa319513d3238796dcffcd7
Kaelan <kbeevans@gmail.com>
2018-10-28 12:49:10 -0600

    readme improvement

--- src/lumps/plaintext/r/Readme.hx
+++ src/lumps/plaintext/r/Readme.hx
@@ -1,5 +1,6 @@
 package lumps.plaintext.r;
 
+import h3d.impl.NullDriver;
 import haxe.CallStack;
 import lumps.base.Plaintext;
 import bench.Project;
@@ -10,16 +11,12 @@ import bench.Project;
  */
 class Readme extends Plaintext 
 {
-	var txt_break:String = "===========================================================================\n";
-	var txt_space:String = "\n                          ";
+	var txt_break:String = "===========================================================================\r\n";
+	var txt_space:String = "\r\n                          ";
 	public function new(_lumpname:String, _directory:String="") 
 	{
 		super(_lumpname, _directory);
 	}
-	
-	public var block_a:Bool = false;
-	public var block_b:Bool = false;
-	public var block_c:Bool = false;
 	
 	//block a
 	public var archive_msg:Null<String>;
@@ -36,7 +33,7 @@ class Readme extends Plaintext
 	public var description:Null<String>;
 	public var credits:Null<String>;
 	//block c
-	public var level_count:Null<Int>;
+	public var level_count:Null<String>;
 	public var sounds:Bool = false;
 	public var music:Bool = false;
 	public var graphics:Bool = false;
@@ -53,68 +50,64 @@ class Readme extends Plaintext
 	public var difficulty:Bool = false;
 	public var base:Null<String>;
 	public var buildtime:Null<String>;
-	public var editors:Null<String> = "";
+	public var editors:Null<String>;
 	public var bugs:Null<String>;
 	public var wont_run:Null<String>;
 	public var tested:Null<String>;
 	override public function compile() 
 	{
 		super.compile();
-		if (block_a) {
-			blocktext += txt_break;
-			if (archive_msg != null) blocktext += 'Archive Maintainer      : ' + parse(archive_msg) + "\n";
-			if (update_to != null) blocktext += 'Archive Maintainer      : ' + parse(update_to) + "\n";
-			blocktext += 'Advanced engine needed  : ' + (advanced_engine == true ? "Yes" : "No") + "\n";
-			blocktext += 'Primary purpose         : ' + primary_purpose + "\n";
-			blocktext += txt_break;
+		blocktext += txt_break;
+		if (archive_msg != null) blocktext += 'Archive Maintainer      : ' + parse(archive_msg) + "\r\n";
+		if (update_to != null) blocktext += 'Archive Maintainer      : ' + parse(update_to) + "\r\n";
+		blocktext += 'Advanced engine needed  : ' + (advanced_engine == true ? "Yes" : "No") + "\r\n";
+		blocktext += 'Primary purpose         : ' + parse(primary_purpose) + "\r\n";
+		blocktext += txt_break;
+		if (title != null) blocktext += 'Title                   : ' + parse(title) + "\r\n";
+		if (wadfilename != null) {
+			blocktext += 'Filename                : ' + parse(wadfilename) + ".pk3" + "\r\n";
+			Project.export_name = wadfilename;
 		}
-		if (block_b) {
-			if (title != null) blocktext += 'Title                   : ' + title + "\n";
-			if (wadfilename != null) {
-				blocktext += 'Filename                : ' + wadfilename + ".pk3" + "\n";
-				Project.export_name = wadfilename;
-			}
-			blocktext += 'Release date            : ' + Date.now() + "\n";
-			if (author != null) blocktext += 'Author                  : ' + author + "\n";
-			if (email != null) blocktext += 'Email Address           : ' + email + "\n";
-			if (other != null) blocktext += 'Other Files By Author   : ' + other + "\n";
-			if (misc != null) blocktext += 'Misc. Author Info       : ' + misc + "\n";
-			blocktext +=  "\n";
-			if (description != null) blocktext += 'Description             : ' + parse(description) + "\n";
-			blocktext +=  "\n";
-			if (credits != null) blocktext += 'Description             : ' + credits + "\n";
-			blocktext += txt_break;
-		}
-		if (block_c) {
-			blocktext += '* What is included *\n\n';
-			if (level_count != null) blocktext += 'New levels              : ' + level_count + "\n";
-			blocktext += 'Sounds                  : ' + (sounds == true ? "Yes" : "No") + "\n";
-			blocktext += 'Music                   : ' + (music == true ? "Yes" : "No") + "\n";
-			blocktext += 'Graphics                : ' + (graphics == true ? "Yes" : "No") + "\n";
-			blocktext += 'Dehacked/BEX Patch      : ' + (dehacked == true ? "Yes" : "No") + "\n";
-			blocktext += 'Demos                   : ' + (demos == true ? "Yes" : "No") + "\n";
-			if (other_b != null) blocktext += 'Other                   : ' + other_b + "\n";
-			else blocktext += 'Other                   : No\n';
-			if (other_c != null) blocktext += 'Other                   : ' + other_c + "\n";
-			else blocktext += 'Other files required    : No\n\n\n';
-			blocktext += '* Play Information *n\n';
-			if (game != null) blocktext += 'Game                    : ' + game + "\n";
-			if (map != null) blocktext += 'Map #                   : ' + map + "\n";
-			if (single_player != null) blocktext += 'Single Player           : ' + single_player + "\n";
-			if (cooperative != null) blocktext += 'Cooperative 2-4 Player  : ' + cooperative + "\n";
-			if (deathmatch != null) blocktext += 'Deathmatch 2-4 Player   : ' + deathmatch + "\n";
-			if (other_game != null) blocktext += 'Other game styles       : ' + other_game + "\n";
-			blocktext += 'Difficulty Settings     : ' + (difficulty == true ? "Yes" : "No") + "\n\n\n";
-			blocktext += '* Construction *\n\n';
-			if (base != null) blocktext += 'Base                    : ' + base + "\n";
-			if (buildtime != null) blocktext += 'Build Time              : ' + buildtime + "\n";
-			blocktext += 'Editor(s) used          : ' + editors + ", ZSDK" + "\n";
-			if (bugs != null) blocktext += 'Known Bugs              : ' + bugs + "\n";
-			if (wont_run != null) blocktext += 'May Not Run With        : ' + wont_run + "\n";
-			if (tested != null) blocktext += 'Tested With             : ' + tested + "\n";
-		}
+		blocktext += 'Release date            : ' + Date.now() + "\r\n";
+		if (author != null) blocktext += 'Author                  : ' + parse(author) + "\r\n";
+		if (email != null) blocktext += 'Email Address           : ' + parse(email) + "\r\n";
+		if (other != null) blocktext += 'Other Files By Author   : ' + parse(other) + "\r\n";
+		if (misc != null) blocktext += 'Misc. Author Info       : ' + parse(misc) + "\r\n";
+		blocktext +=  "\r\n";
+		if (description != null) blocktext += 'Description             : ' + parse(description) + "\r\n";
+		blocktext +=  "\r\n";
+		if (credits != null) blocktext += 'Additional Credits to   : ' + parse(credits) + "\r\n";
+		blocktext += txt_break;
+		blocktext += '* What is included *\r\n\r\n';
+		if (level_count != null) blocktext += 'New levels              : ' + parse(level_count) + "\r\n";
+		blocktext += 'Sounds                  : ' + (sounds == true ? "Yes" : "No") + "\r\n";
+		blocktext += 'Music                   : ' + (music == true ? "Yes" : "No") + "\r\n";
+		blocktext += 'Graphics                : ' + (graphics == true ? "Yes" : "No") + "\r\n";
+		blocktext += 'Dehacked/BEX Patch      : ' + (dehacked == true ? "Yes" : "No") + "\r\n";
+		blocktext += 'Demos                   : ' + (demos == true ? "Yes" : "No") + "\r\n";
+		if (other_b != null) blocktext += 'Other                   : ' + parse(other_b) + "\r\n";
+		else blocktext += 'Other                   : No\r\n';
+		if (other_c != null) blocktext += 'Other                   : ' + parse(other_c) + "\r\n";
+		else blocktext += 'Other files required    : No\r\n\r\n\r\n';
+		blocktext += '* Play Information *\r\n';
+		if (game != null) blocktext += 'Game                    : ' + parse(game) + "\r\n";
+		if (map != null) blocktext += 'Map #                   : ' + parse(map) + "\r\n";
+		if (single_player != null) blocktext += 'Single Player           : ' + parse(single_player) + "\r\n";
+		if (cooperative != null) blocktext += 'Cooperative 2-4 Player  : ' + parse(cooperative) + "\r\n";
+		if (deathmatch != null) blocktext += 'Deathmatch 2-4 Player   : ' + parse(deathmatch) + "\r\n";
+		if (other_game != null) blocktext += 'Other game styles       : ' + parse(other_game) + "\r\n";
+		blocktext += 'Difficulty Settings     : ' + (difficulty == true ? "Yes" : "No") + "\r\n\r\n\r\n";
+		blocktext += '* Construction *\r\n\r\n';
+		if (base != null) blocktext += 'Base                    : ' + parse(base) + "\r\n";
+		if (buildtime != null) blocktext += 'Build Time              : ' + parse(buildtime) + "\r\n";
+		if (editors != null) blocktext += 'Editor(s) used          : ' + parse(editors) + ", ZSDK" + "\r\n";
+		else blocktext += 'Editor(s) used          : ZSDK' + "\r\n";
+		if (bugs != null) blocktext += 'Known Bugs              : ' + parse(bugs) + "\r\n";
+		if (wont_run != null) blocktext += 'May Not Run With        : ' + parse(wont_run) + "\r\n";
+		if (tested != null) blocktext += 'Tested With             : ' + parse(tested) + "\r\n";
 	}
-	function parse(_message:String):String { //returns a formatted string for the readme.txt file that's typically shipped with id Games' hosted mods.
+	function parse(_message:Null<String>):String { //returns a formatted string for the readme.txt file that's typically shipped with id Games' hosted mods.
+		if (_message == null) return null;
 		var _locString:String = "";
 		var b = 0;
 		for (a in 0..._message.length) {

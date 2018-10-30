bdbca0c44e57010cf321bdc13d31ab9b4a858a81
Kaelan <kbeevans@gmail.com>
2018-10-30 01:33:09 -0600

    Re-added previous lump code

---
+++ src/lumps/plaintext/c/Cvarinfo.hx
@@ -0,0 +1,18 @@
+package lumps.plaintext.c;
+
+import lumps.base.Plaintext;
+
+/**
+ * ...
+ * @author kevansevans
+ */
+class Cvarinfo extends Plaintext 
+{
+
+	public function new(_lumpname:String, _directory:String="") 
+	{
+		super(_lumpname, _directory);
+		
+	}
+	
+}
---
+++ src/lumps/plaintext/d/Dehsupp.hx
@@ -0,0 +1,18 @@
+package lumps.plaintext.d;
+
+import lumps.base.Plaintext;
+
+/**
+ * ...
+ * @author kevansevans
+ */
+class Dehsupp extends Plaintext 
+{
+
+	public function new(_lumpname:String, _directory:String="") 
+	{
+		super(_lumpname, _directory);
+		
+	}
+	
+}
